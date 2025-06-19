4b55b8e5e48475a5b77706d8db23fcc3569699b2
Yusk1450 <ishigo@4dpocket.co.jp>
2025-06-20 01:37:31 +0900

    Initial Commit

---
+++ progland/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  progland
+//
+//  Created by ISHIGO Yusuke on 2025/06/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
